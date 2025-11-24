0b34f42ded236e99e87fb51e50d41768338660d9
ShawnSmith7 <shawnsmithsbs7@gmail.com>
2025-11-24 18:51:15 -0500

    Hello Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,116 @@
 #include "Window.h"
 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+#ifndef SRC_DIR
+#define SRC_DIR "./"
+#endif
+
 void processInput(GLFWwindow* window);
 
 int main() {
     Window window(1280, 800, "GraphicsEngine2D");
 
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    float vertices[] = {
+        -0.5f, 0.5f,
+        -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.5f, 0.5f
+    };
+
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    unsigned int ebo;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+
+    const char* vertexShaderFilePath = SRC_DIR "vertex.shader";
+    std::ifstream vertexShaderFile(vertexShaderFilePath);
+    if (!vertexShaderFile.is_open())
+        std::cerr <<"Failed to open " << vertexShaderFilePath << '\n';
+    
+    std::ostringstream vertexShaderBuffer;
+    vertexShaderBuffer << vertexShaderFile.rdbuf();
+    std::string vertexShaderString = vertexShaderBuffer.str();
+    const char* vertexShaderCString = vertexShaderString.c_str();
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderCString, 0);
+    glCompileShader(vertexShader);
+
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << '\n';
+    }
+
+    const char* fragmentShaderFilePath = SRC_DIR "fragment.shader";
+    std::ifstream fragmentShaderFile(fragmentShaderFilePath);
+    if (!fragmentShaderFile.is_open())
+        std::cerr << "Failed to open " << fragmentShaderFilePath;
+
+    std::ostringstream fragmentShaderBuffer;
+    fragmentShaderBuffer << fragmentShaderFile.rdbuf();
+    std::string fragmentShaderString = fragmentShaderBuffer.str();
+    const char* fragmentShaderCString = fragmentShaderString.c_str();
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderCString, 0);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << '\n';
+    }
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << '\n';
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glUseProgram(shaderProgram);
+
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
     window.render([&]() {
         processInput(window.get());
 
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     });
     
     glfwTerminate();
