0836dff0fb9d200a51fd1ea2b0bb238b37308743
ShawnSmith7 <shawnsmithsbs7@gmail.com>
2025-11-28 02:04:58 -0500

    update Line class

--- src/Line.cpp
+++ src/Line.cpp
@@ -2,9 +2,7 @@
 
 Line::Line(const glm::vec2& pos1, const glm::vec2& pos2, float width, const glm::vec4& color) :
     pos2(pos2), width(width), color(color) {
-    transform.setTranslation(pos1);
-    transform.setScaling(glm::vec2(glm::length(pos2 - pos1), 1.0f));
-    transform.setRotation(atan2(pos2.y - pos1.y, pos2.x - pos1.x));
+    updateTransform(pos1, pos2);
     
     static bool initialized = false;
     if (!initialized) {
@@ -21,6 +19,39 @@ Line::Line(const glm::vec2& pos1, const glm::vec2& pos2, float width, const glm:
 
         vertexArray.unbind();
     }
+}
+
+glm::vec2 Line::getPos1() const {
+    return transform.getTranslation();
+}
+
+glm::vec2 Line::getPos2() const {
+    return pos2;
+}
+
+float Line::getWidth() const {
+    return width;
+}
+
+glm::vec4 Line::getColor() const {
+    return color;
+}
+
+void Line::setPos1(const glm::vec2& pos1) {
+    updateTransform(pos1, pos2);
+}
+
+void Line::setPos2(const glm::vec2& pos2) {
+    this->pos2 = pos2;
+    updateTransform(getPos1(), pos2);
+}
+
+void Line::setWidth(float width) {
+    this->width = width;
+}
+
+void Line::setColor(const glm::vec4& color) {
+    this->color = color;
 }
 
 void Line::draw(const ShaderProgram& shaderProgram) {
@@ -40,3 +71,9 @@ VertexArray Line::vertexArray;
 VertexBuffer Line::vertexBuffer;
 
 const float Line::vertices[] = { 0.0f, 0.0f, 1.0f, 0.0f };
+
+void Line::updateTransform(const glm::vec2& pos1, const glm::vec2& pos2) {
+    transform.setTranslation(pos1);
+    transform.setScaling(glm::vec2(glm::length(pos2 - pos1), 1.0f));
+    transform.setRotation(atan2(pos2.y - pos1.y, pos2.x - pos1.x));
+}
--- src/Line.h
+++ src/Line.h
@@ -9,6 +9,16 @@ class Line : public Drawable {
             float width = 1.0f,
             const glm::vec4& color = glm::vec4(1.0f));
 
+        glm::vec2 getPos1() const;
+        glm::vec2 getPos2() const;
+        float getWidth() const;
+        glm::vec4 getColor() const;
+
+        void setPos1(const glm::vec2& pos1);
+        void setPos2(const glm::vec2& pos2);
+        void setWidth(float width);
+        void setColor(const glm::vec4& color);
+
         void draw(const ShaderProgram& shaderProgram) override;
     protected:
         Line(const glm::vec2& pos2 = glm::vec2(0.0f, 1.0f),
@@ -23,4 +33,6 @@ class Line : public Drawable {
         static VertexBuffer vertexBuffer;
 
         static const float vertices[4];
+
+        void updateTransform(const glm::vec2& pos1, const glm::vec2& pos2);
 };
