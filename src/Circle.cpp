8890e9e06bd359f3baa04e65ca279ae64a22dc06
ShawnSmith7 <shawnsmithsbs7@gmail.com>
2025-11-29 18:01:02 -0500

    Circle triangle fan optomization

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -63,7 +63,7 @@ void Circle::draw(const ShaderProgram& shaderProgram) {
     vertexArray.bind();
     shaderProgram.setMat4("model", transform.getMatrix());
     shaderProgram.setVec4("color", color);
-    glDrawElements(GL_TRIANGLES, indexBuffer.getCount(), GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLE_FAN, indexBuffer.getCount(), GL_UNSIGNED_INT, 0);
 }
 
 void Circle::genGeometry() {
@@ -85,13 +85,11 @@ void Circle::genGeometry() {
     vertexBuffer.bind();
     vertexBuffer.setData(vertices, GL_STATIC_DRAW);
 
-    indices.resize(3 * resolution);
+    indices.resize(resolution + 2);
 
-    for (unsigned int i = 0; i < resolution; i++) {
-        indices[3 * i] = 0;
-        indices[3 * i + 1] = i + 1;
-        indices[3 * i + 2] = (i + 1) % resolution + 1;
-    }
+    for (unsigned int i = 0; i < resolution + 1; i++)
+        indices[i] = i;
+    indices[resolution + 1] = 1;
 
     indexBuffer.gen();
     indexBuffer.bind();
